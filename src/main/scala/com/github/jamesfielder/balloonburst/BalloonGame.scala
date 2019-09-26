f6be65e29475a9e66fb588204d1862b7f6ba06ae
James Fielder <james@fielder.dev>
2019-09-26 11:21:22 +0100

    Working taking input from the console

--- src/main/scala/com/github/jamesfielder/balloonburst/BalloonGame.scala
+++ src/main/scala/com/github/jamesfielder/balloonburst/BalloonGame.scala
@@ -1,24 +1,30 @@
 package com.github.jamesfielder.balloonburst
 
 import scala.io.StdIn.readLine
-import scala.util.Try
+import scala.util.{Success, Try}
 import com.github.jamesfielder.balloonburst.ExceptionFunctions.exitWithMessage
 
 object BalloonGame extends App {
 
-  val balloons: Try[Seq[Int]] = for {
+  type Burst = String
+
+  val balloonsTry: Try[Seq[Int]] = for {
     line <- Try(readLine())
-    split <- Try(line.split(" "))
+    split <- Try(line.split(" ")).filter(_.length > 0)
     splitSeq <- Try(split.toSeq)
-    nums <- Try(splitSeq.map(_.toInt))
+    nums <- Try(splitSeq.map(_.toInt)).filter(_.forall(_ > 0))
   } yield nums
 
-  balloons.recover {
+  balloonsTry.recover {
+    case n: NoSuchElementException => exitWithMessage("Make sure that all of your balloon inflation limits are greater than 0", None)
     case e: NumberFormatException => exitWithMessage("Unable to parse input into balloon pump limit", Some(e))
+    case _ => exitWithMessage("Unknown failure, exiting", None)
   }
 
-  println(balloons)
-}
+  // Since we've used recover above we can now safely call .get on the Try
+  balloonsTry.get.map(b => {
 
-class BalloonGame {
+  })
+
+
 }
